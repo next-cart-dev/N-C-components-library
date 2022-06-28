0e145571df5b3a50312601720d3f266305d19e33
DehLuizSantos <and_consul@outlook.com>
2022-06-28 10:26:27 -0300

    fix(types):try

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-export type ButtonProps = {
+export interface ButtonProps {
     text:string;
 }
 
 
  const Button: React.FC<ButtonProps> = ({text}) =>{
     return(
-        <button   style={{
-        backgroundColor: 'blueviolet',
-        color: 'white',
-        fontSize: '17px',
-        padding: '10px 16px',
-        borderRadius: '99999px',
-      }}>
+        <button>
             {text}
         </button>
     )
