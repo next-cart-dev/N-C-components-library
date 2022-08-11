ff733cf85d3bb54acfa76e10b80331786bb7140c
DehLuizSantos <and_consul@outlook.com>
2022-08-11 11:46:14 -0300

    fix: expoort components index

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
 export { styled, getCssText } from "./stitches.config"
 
 export { Button } from "./Button"
+
+export { Checkbox } from "./CheckBox"
+
+export { RadioGroup } from "./RadioGroup"
+
+export { Switch } from "./Switch"
