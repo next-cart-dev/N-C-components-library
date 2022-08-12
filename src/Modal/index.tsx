60eb53f3872e32a4e810f24b1f3b44ec6f0c4eed
Pedro Henrique <pedro@cartco.com.br>
2022-08-12 11:54:18 -0300

    chore: reviewing some things

--- src/Modal/index.tsx
+++ src/Modal/index.tsx
@@ -1,25 +1,26 @@
 import React from "react"
-import * as Dialog from '@radix-ui/react-dialog';
-import * as S from './styles';
-import { Props } from "./types";
+
+import * as Dialog from "@radix-ui/react-dialog"
+
+import * as S from "./styles"
+import { Props } from "./types"
 
 function Content({ children, ...props }: any) {
-    return (
-        <Dialog.Portal>
-            <S.StyledOverlay />
-            <S.DialogContent {...props}>{children}</S.DialogContent>
-        </Dialog.Portal>
-    );
+  return (
+    <Dialog.Portal>
+      <S.StyledOverlay />
+      <S.DialogContent {...props}>{children}</S.DialogContent>
+    </Dialog.Portal>
+  )
 }
 const DialogContent = Content
 
 export const Modal = ({ trigger, children, open, onOpenChange }: Props) => {
-    return (
-        <Dialog.Root open={open} onOpenChange={onOpenChange}>
-            <Dialog.Trigger asChild children={trigger} />
-            <DialogContent>
-                {children}
-            </DialogContent>
-        </Dialog.Root>
-    )
+  return (
+    <Dialog.Root open={open} onOpenChange={onOpenChange}>
+      {/* TODO: Ajustar erro de tipagem, children deve ser passado no miolo do componente e não como props */}
+      <Dialog.Trigger asChild children={trigger} />
+      <DialogContent>{children}</DialogContent>
+    </Dialog.Root>
+  )
 }
--- src/Modal/story.tsx
+++ src/Modal/story.tsx
@@ -1,22 +1,50 @@
+import React, { useState } from "react"
+
+import * as S from "./styles"
+
+import { Button } from "../Button"
+
 import { Modal } from "."
-import { Button } from '../Button'
-import React, { useState } from "react";
-import * as S from './styles';
+
+// TODO: Criar componentes para o corpo e ações do modal
+// - ModalBody, que já tem a estilização de display: flex e etc, conforme a linha 23
+// - ModalActions, também com display flex e etc, conforme a linha 35 (no marginTop, utilizar o token de espaçamento mais próximo de 30px)
+// Assim não é necessário definir esses estilos na hora de utilizar o modal
 
 export const ModalExample = () => {
-    const [open, setOpen] = useState(false)
-    return (
-        <>
-            <Modal open={open} onOpenChange={setOpen} trigger={<Button>Click Me to open the modal</Button>}>
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-                    <S.DialogTitle>Titulo Do Modal</S.DialogTitle>
-                    <S.DialogDescription>Texto para acompanhar modal de exemplo</S.DialogDescription>
-                </div>
-                <div style={{ display: 'flex', gap: '4rem', marginTop: '30px' }}>
-                    <S.ModalButton onClick={() => setOpen(false)} variant='primary'>Button</S.ModalButton>
-                    <S.ModalButton onClick={() => setOpen(false)} variant='secondary'>Button</S.ModalButton>
-                </div>
-            </Modal>
-        </>
-    )
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      <Modal
+        open={open}
+        onOpenChange={setOpen}
+        trigger={<Button>Click Me to open the modal</Button>}
+      >
+        {/* TODO: <ModalBody/> */}
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column"
+          }}
+        >
+          <S.DialogTitle>Titulo Do Modal</S.DialogTitle>
+          <S.DialogDescription>
+            Texto para acompanhar modal de exemplo
+          </S.DialogDescription>
+        </div>
+        {/* TODO: <ModalActions/> */}
+        <div style={{ display: "flex", gap: "4rem", marginTop: "30px" }}>
+          {/* TODO: Não é necessário criar novos botões, o componente a ser utilizado é o Button mesmo */}
+          <S.ModalButton onClick={() => setOpen(false)} variant="primary">
+            Button
+          </S.ModalButton>
+          <S.ModalButton onClick={() => setOpen(false)} variant="secondary">
+            Button
+          </S.ModalButton>
+        </div>
+      </Modal>
+    </>
+  )
 }
