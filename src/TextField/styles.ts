fd27df643f7e255cae0730e45f3a49b33c2aa43b
Pedro Henrique <pedro@cartco.com.br>
2022-08-17 14:44:53 -0300

    feat(nxc-75): added new variants

--- src/TextField/styles.ts
+++ src/TextField/styles.ts
@@ -1,22 +1,24 @@
 import { styled } from "../stitches.config"
 
 export const ADORNMENT_PADDING = 12
-export const INPUT_SIZE = 300
+export const INPUT_SIZE = "300px"
 
 export const Input = styled("input", {
   $$borderColor: "$colors$primary700",
   $$disabledBorderColor: "$colors$text400",
   $$dangerBorderColor: "$colors$danger500",
   $$successBorderColor: "$colors$tertiary500",
+  $$infoBorderColor: "$colors$info500",
+  $$warningBorderColor: "$colors$warning500",
   padding: "14px $16",
   fontSize: "$18",
-  height: 48,
+  height: "48px",
   background: "$neutral200",
   color: "$text700",
   fontWeight: "$normal",
   letterSpacing: "$0.5",
   border: "none",
-  borderRadius: 4,
+  borderRadius: "4px",
   boxShadow: "0 0 0 1px $$borderColor",
   outline: "none",
   transition: "box-shadow 0.3s ease-in-out, background 0.1s ease-in-out",
@@ -45,6 +47,9 @@ export const Input = styled("input", {
     variant: {
       success: {
         boxShadow: "0 0 0 1px $$successBorderColor",
+        "&:hover": {
+          background: "$success200"
+        },
         "&:focus-visible": {
           boxShadow: "0 0 0 2px $$successBorderColor"
         },
@@ -54,11 +59,38 @@ export const Input = styled("input", {
       },
       danger: {
         boxShadow: "0 0 0 1px $$dangerBorderColor",
+        "&:hover": {
+          background: "$danger200"
+        },
         "&:focus-visible": {
           boxShadow: "0 0 0 2px $$dangerBorderColor"
         },
         "&::placeholder": {
           color: "$danger500"
+        }
+      },
+      info: {
+        boxShadow: "0 0 0 1px $$infoBorderColor",
+        "&:hover": {
+          background: "$info200"
+        },
+        "&:focus-visible": {
+          boxShadow: "0 0 0 2px $$infoBorderColor"
+        },
+        "&::placeholder": {
+          color: "$info500"
+        }
+      },
+      warning: {
+        boxShadow: "0 0 0 1px $$warningBorderColor",
+        "&:hover": {
+          background: "$warning200"
+        },
+        "&:focus-visible": {
+          boxShadow: "0 0 0 2px $$warningBorderColor"
+        },
+        "&::placeholder": {
+          color: "$warning500"
         }
       }
     }
@@ -81,6 +113,12 @@ export const InputAdornment = styled("div", {
       },
       danger: {
         backgroundColor: "$danger500"
+      },
+      warning: {
+        backgroundColor: "$warning500"
+      },
+      info: {
+        backgroundColor: "$info500"
       }
     }
   }
