921e2448fbe408326931373b2a7c81cafbc6a27b
Pedro Henrique <pedro@cartco.com.br>
2022-08-18 12:19:06 -0300

    chore: organizing components in folder

---
+++ src/components/Icon/styles.ts
@@ -0,0 +1,39 @@
+import { styled } from "../../stitches.config"
+
+export const Icon = styled("span", {
+  display: "inherit",
+  variants: {
+    variant: {
+      primary: {
+        "& svg": {
+          fill: "$neutral100"
+        }
+      },
+      secondary: {
+        "& svg": {
+          fill: "$secondary400"
+        }
+      }
+    },
+    size: {
+      small: {
+        "& svg": {
+          width: "12px",
+          height: "12px"
+        }
+      },
+      normal: {
+        "& svg": {
+          width: "16px",
+          height: "16px"
+        }
+      },
+      large: {
+        "& svg": {
+          width: "24px",
+          height: "24px"
+        }
+      }
+    }
+  }
+})
