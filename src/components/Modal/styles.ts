e465cf854f4146782a335c742c99065dc7d68545
Andrey Soares <andrey.soares.oliveira4742@gmail.com>
2022-08-24 16:32:54 -0300

    refactor: style changes

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,5 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog"
-import { GrClose } from 'react-icons/all'
+import { GrClose } from 'react-icons/gr'
 
 import { styled, keyframes } from "../../stitches.config"
 
@@ -31,8 +31,7 @@ export const DialogContent = styled(Dialog.Content, {
   justifyContent: "center",
   alignItems: "center",
   flexDirection: "column",
-  boxShadow:
-    "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
+  boxShadow: "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
   "@media (prefers-reduced-motion: no-preference)": {
     animation: `${contentShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`
   },
