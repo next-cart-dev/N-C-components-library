2da5a53e5c0cdfbe88d3222625ec0325581b866b
Pedro Henrique <pedro@cartco.com.br>
2022-09-19 15:58:55 -0300

    refactor: removed color prop in favor of css prop

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -3,8 +3,8 @@ import React from "react"
 import * as S from "./styles"
 import { Props } from "./types"
 
-export const Icon = ({ size, icon, color = "$neutral100", css }: Props) => (
-  <S.Icon size={size} css={css} color={color}>
+export const Icon = ({ size, icon, css }: Props) => (
+  <S.Icon size={size} css={{ ...css }}>
     {icon}
   </S.Icon>
 )
--- src/components/Icon/types.ts
+++ src/components/Icon/types.ts
@@ -5,6 +5,5 @@ import { config } from "../../stitches.config"
 export type Props = {
   size: "small" | "normal" | "large"
   icon: React.ReactNode
-  color: string
   css?: CSS<typeof config>
 }
