4b5b27af191632518e66b12b0b7a8ea737cda4da
Andrey Soares <andrey.soares.oliveira4742@gmail.com>
2022-09-28 14:55:06 -0300

    perf: adding css prop in Card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,6 +3,10 @@ import React from "react"
 import * as S from "./styles"
 import { Props } from "./types"
 
-export const Card = ({ children, variant = "primary" }: Props) => {
-  return <S.Card variant={variant}>{children}</S.Card>
+export const Card = ({ children, variant = "primary", css }: Props) => {
+  return (
+    <S.Card css={{ ...css }} variant={variant}>
+      {children}
+    </S.Card>
+  )
 }
