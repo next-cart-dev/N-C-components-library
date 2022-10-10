e3b2cacbdc89f27e6350d3a2c080bb70ef32d91f
Andrey Soares <andrey.soares.oliveira4742@gmail.com>
2022-10-10 13:24:58 -0300

    refactor: changes on types

--- src/components/TextFieldMask/types.tsx
+++ src/components/TextFieldMask/types.tsx
@@ -2,13 +2,4 @@ import { ComponentPropsWithoutRef } from "react"
 
 import { Props as PatternProps } from "./StaticMask/types"
 
-export type MaskType = "cpf" | "cnpj" | "date" | "mobile"
-
-export type MaskFormat = {
-  format: string
-  placeholder: string
-}
-
-export type DefaultMaskMapping = Record<MaskType, MaskFormat>
-
 export type Props = ComponentPropsWithoutRef<"input"> & PatternProps
