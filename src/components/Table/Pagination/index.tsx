8811632dc60162c9c1541e5dab8c2afcc28602f9
Pedro Henrique <pedro@cartco.com.br>
2022-11-07 12:31:35 -0300

    chore: added logs for debugging

--- src/components/Table/Pagination/index.tsx
+++ src/components/Table/Pagination/index.tsx
@@ -5,7 +5,7 @@ import { BsFillCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs"
 
 import { Box } from "../../Box"
 import { Icon } from "../../Icon"
-import { Typography } from "../../Typography"
+// import { Typography } from "../../Typography"
 import * as S from "../styles"
 
 type Props = {
@@ -14,37 +14,40 @@ type Props = {
 }
 
 export const TablePagination = ({ table, totalCount }: Props) => {
-  const currentPage = table.getState().pagination.pageIndex + 1
+  const pageIndex = table.getState().pagination.pageIndex
+  const currentPage = pageIndex + 1
 
-  const CountView = () => {
-    const paginatedCount = `${table.getPaginationRowModel().rows[0].index + 1} -
-    ${
-      table.getPaginationRowModel().rows[
-        table.getPaginationRowModel().rows.length - 1
-      ].index + 1
-    }`
+  console.log(table.getPaginationRowModel())
+  console.log(pageIndex)
+  // const CountView = () => {
+  //   const paginatedCount = `${table.getPaginationRowModel().rows[0].index + 1} -
+  //   ${
+  //     table.getPaginationRowModel().rows[
+  //       table.getPaginationRowModel().rows.length - 1
+  //     ].index + 1
+  //   }`
 
-    return (
-      <Typography
-        css={{
-          letterSpacing: "1.2px",
-          fontSize: "$16",
-          color: "$text200",
-          marginBottom: "$16",
-          "@media (min-width: 768px)": {
-            marginBottom: "0",
-            fontSize: "$18"
-          }
-        }}
-      >
-        Resultado: {paginatedCount} de {totalCount}
-      </Typography>
-    )
-  }
+  //   return (
+  //     <Typography
+  //       css={{
+  //         letterSpacing: "1.2px",
+  //         fontSize: "$16",
+  //         color: "$text200",
+  //         marginBottom: "$16",
+  //         "@media (min-width: 768px)": {
+  //           marginBottom: "0",
+  //           fontSize: "$18"
+  //         }
+  //       }}
+  //     >
+  //       Resultado: {paginatedCount} de {totalCount}
+  //     </Typography>
+  //   )
+  // }
 
   return (
     <S.Pagination>
-      <CountView />
+      {/* <CountView /> */}
       <Box css={{ display: "flex", flexFlow: "row" }}>
         <S.PaginationButton
           type="button"
