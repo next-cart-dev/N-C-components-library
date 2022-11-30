830e221813795c1380748d7f9a2bb2e2370a6934
Andrey Soares <andrey.soares.oliveira4742@gmail.com>
2022-11-29 21:29:15 -0300

    feat: adding error message on Select + variants

--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -1,6 +1,6 @@
 import { StylesConfig } from "react-select"
 
-import { theme } from "../../stitches.config"
+import { styled, theme } from "../../stitches.config"
 
 const colors = theme.colors
 
@@ -41,3 +41,122 @@ export const SelectStyled: StylesConfig = {
     }
   })
 }
+
+export const DangerVariant: StylesConfig = {
+  ...SelectStyled,
+  control: (styles) => ({
+    ...styles,
+    borderRadius: "4px",
+    borderColor: colors.danger500.value,
+    height: "48px"
+  }),
+  container: (styles) => ({ ...styles, borderRadius: "4px", width: "100%" }),
+  dropdownIndicator: (styles) => ({
+    ...styles,
+    background: colors.danger500.value,
+    borderRadius: "0 2px 2px 0",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    padding: "0 10px",
+    alignItems: "center"
+  }),
+  multiValueRemove: (styles) => ({
+    ...styles,
+    "&:hover": {
+      background: colors.danger500.value,
+      color: colors.text200.value
+    }
+  })
+}
+
+export const InfoVariant: StylesConfig = {
+  ...SelectStyled,
+  control: (styles) => ({
+    ...styles,
+    borderRadius: "4px",
+    borderColor: colors.info500.value,
+    height: "48px"
+  }),
+  container: (styles) => ({ ...styles, borderRadius: "4px", width: "100%" }),
+  dropdownIndicator: (styles) => ({
+    ...styles,
+    background: colors.info500.value,
+    borderRadius: "0 2px 2px 0",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    padding: "0 10px",
+    alignItems: "center"
+  }),
+  multiValueRemove: (styles) => ({
+    ...styles,
+    "&:hover": {
+      background: colors.info500.value,
+      color: colors.text200.value
+    }
+  })
+}
+
+export const WarningVariant: StylesConfig = {
+  ...SelectStyled,
+  control: (styles) => ({
+    ...styles,
+    borderRadius: "4px",
+    borderColor: colors.warning500.value,
+    height: "48px"
+  }),
+  container: (styles) => ({ ...styles, borderRadius: "4px", width: "100%" }),
+  dropdownIndicator: (styles) => ({
+    ...styles,
+    background: colors.warning500.value,
+    borderRadius: "0 2px 2px 0",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    padding: "0 10px",
+    alignItems: "center"
+  }),
+  multiValueRemove: (styles) => ({
+    ...styles,
+    "&:hover": {
+      background: colors.warning500.value,
+      color: colors.text200.value
+    }
+  })
+}
+
+export const SuccessVariant: StylesConfig = {
+  ...SelectStyled,
+  control: (styles) => ({
+    ...styles,
+    borderRadius: "4px",
+    borderColor: colors.tertiary500.value,
+    height: "48px"
+  }),
+  container: (styles) => ({ ...styles, borderRadius: "4px", width: "100%" }),
+  dropdownIndicator: (styles) => ({
+    ...styles,
+    background: colors.tertiary500.value,
+    borderRadius: "0 2px 2px 0",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    padding: "0 10px",
+    alignItems: "center"
+  }),
+  multiValueRemove: (styles) => ({
+    ...styles,
+    "&:hover": {
+      background: colors.tertiary500.value,
+      color: colors.text200.value
+    }
+  })
+}
+
+export const ErrorMessage = styled("p", {
+  color: "$danger500",
+  margin: "6px 0 0 10px",
+  fontSize: "12px",
+  height: "12px"
+})
