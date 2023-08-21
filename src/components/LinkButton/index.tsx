1c5be6edbccee48882dbbc97742011f15e2fde93
Pedro Henrique <pedro@cartco.com.br>
2023-08-21 09:22:07 -0300

    chore: removed element

--- src/components/LinkButton/index.tsx
+++ src/components/LinkButton/index.tsx
@@ -3,12 +3,6 @@ import React from "react"
 import * as S from "./styles"
 import { Props } from "./types"
 
-import { Box } from "../Box"
-
 export const LinkButton = ({ ...props }: Props) => {
-  return (
-    <Box>
-      <S.LinkButton {...props}>{props.children}</S.LinkButton>
-    </Box>
-  )
+  return <S.LinkButton {...props}>{props.children}</S.LinkButton>
 }
